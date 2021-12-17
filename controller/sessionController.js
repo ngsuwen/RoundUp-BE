9ce597145ad538f74952ca7b245e820031f32291
LAPTOP-KH9KVP83\Suwen <ng.suwen@dhs.sg>
2021-12-17 11:49:31 +0800

    connected refreshtoken to database

--- controller/sessionController.js
+++ controller/sessionController.js
@@ -1,60 +1,80 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 // schema
-const User = require('../models/user');
-const Token = require('../models/token');
+const User = require("../models/user");
+const Token = require("../models/token");
 
 router.use(express.json());
 
-// to be stored in database
-let refreshTokens;
-let accessToken;
-let refreshToken;
-
-router.post("/login", async(req, res) => {
+router.post("/login", async (req, res) => {
   const user = req.body.username;
-  const password = req.body.password
-  const isUserValid = await User.findOne({username: user});
+  const password = req.body.password;
+  const isUserValid = await User.findOne({ username: user });
   // authenticate user
   if (isUserValid) {
-    const isPasswordValid = await bcrypt.compare(password,isUserValid.password);
-    if (isPasswordValid){
-      const tokens = await Token.create({accessToken: generateAccessToken(user), refreshToken:generateRefreshToken(user)});
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      isUserValid.password
+    );
+    if (isPasswordValid) {
+      const tokens = await Token.create({
+        accessToken: generateAccessToken(user),
+        refreshToken: generateRefreshToken(user),
+      });
       res.send(tokens);
     } else {
       res.status(400).send("invalid password");
     }
   } else {
-    res.status(403).send("invalid user");
+    res.status(400).send("invalid user");
   }
 });
 
 // use refresh token
-router.post("/token", (req, res) => {
-  refreshToken = req.body.token;
-  if (refreshTokens.includes(refreshToken)) {
-    // get new tokens
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-      accessToken = generateAccessToken(user.username);
-      refreshToken = generateRefreshToken(user.username);
-      refreshTokens = refreshToken;
-      res.send({ accessToken, refreshToken });
-    });
+router.post("/token", async (req, res) => {
+  const refreshToken = req.body.token;
+  // check token with database
+  const isTokenValid = await Token.findOne({ refreshToken: refreshToken });
+  if (isTokenValid) {
+    jwt.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET,
+      async (err, user) => {
+        if (err) {
+          // expired
+          res.status(498).send("token expired");
+        } else {
+          // replace tokens
+          const tokens = await Token.findOneAndUpdate(
+            { refreshToken: refreshToken },
+            {
+              accessToken: generateAccessToken(user.username),
+              refreshToken: generateRefreshToken(user.username),
+            },
+            { new: true }
+          );
+          res.send(tokens);
+        }
+      }
+    );
   } else {
-    // expired
-    res.status(403).send("Forbidden");
+    res.status(400).send("invalid token");
   }
 });
 
 function generateAccessToken(user) {
-  return jwt.sign({username: user}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '15s'});
+  return jwt.sign({ username: user }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "10s",
+  });
 }
 
 function generateRefreshToken(user) {
-  return jwt.sign({username: user}, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '15s'});
+  return jwt.sign({ username: user }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "10s",
+  });
 }
 
-module.exports=router;
+module.exports = router;

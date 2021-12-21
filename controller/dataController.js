48730d2cf3701347f3cee046959c004028c1e5b6
qamarularifin <qamarulafro89@gmail.com>
2021-12-21 15:38:26 +0800

    seeding dataCash not working

--- controller/dataController.js
+++ controller/dataController.js
@@ -1,49 +1,64 @@
 // Dependencies
 const express = require("express")
+const mongoose = require("mongoose")
 const router = express.Router()
 
 // Schema
+const User = require("../models/user")
 const DataCash = require("../models/data_cash")
+
 
 // Seeding data
 
-router.post("/cash/seed", async (req,res)=>{
-    const cashInput =[
-        {
-          
-            cashentry: [
-                {
-                    amount: 1700,
-                    category: "income",
-                    description: "bonus"
+// router.post("/cash/seed", async (req,res)=>{
+//     const user = "61bd9a6c2fcd3b08f3365f75"
+//     const cashInput =[
+//         {
+//             username : user,
+//             cashentry: [
+//                 {
+                   
+//                     amount: 1700,
+//                     category: "Income",
+//                     description: "Monthly"
 
-                }
-            ]
-        }
-    ]
+//                 }
+//             ]
+//         }
+//     ]
 
-    try{
-        const seedItems =  await DataCash.create(cashInput)
-        res.send(seedItems)
-    } catch (err){
-        res.send(err.message)
-    }
+//     // let newCash = new DataCash(cashInput);
+//     // newCash.save((err, cash) =>{
+//     //     if(err){
+//     //         res.status(400).send(err)
+//     //     } else{
+//     //         res.status(200).json(cash)
+//     //     }
+//     // })
 
-})
+//     /////////
+//     try{
+//         const seedItems =  await DataCash.create(cashInput)
+//         res.send(seedItems)
+//     } catch (err){
+//         res.send(err.message)
+//     }
+//     /////////
+// })
 
 
 
 // Routes
 // get all cash
 router.get("/cash", async (req,res)=>{
-    const cash = await DataCash.find()
+    const cash = await DataCash.find({})
     res.send(cash)
 })
 
 // show route
 router.get("/cash/:id", async (req,res)=>{
     const {id} = req.params
-    const cash = await DataCash.findById(id)
+    const cash = await DataCash.findById(id).populate("username")
     res.send(cash)
 })
 

8d66a594a7f93aeae5ac07b3950488dfccb7beff
qamarularifin <qamarulafro89@gmail.com>
2021-12-22 19:01:02 +0800

    working on expenseSeeder date

--- seed/expenseSeeder.js
+++ seed/expenseSeeder.js
@@ -11,74 +11,83 @@ const category = ["Shopping", "Food", "Health", "Transportation", "Household"]
 const amount = ["100", "200", "300", "400", "500", "600", "700", "800"]
 const description = ["A", "B", "C", "D", "E", "F", "G", "H"]
 
+// function formatDate(dateObject){
+//     const parts = {
+//         date: dateObject.getDate(),
+   
+//         month: dateObject.getMonth() + 1,
+//         year: dateObject.getFullYear()
+//     }
+//     //console.log(parts)
+//     return `${parts.date}/${parts.month}/${parts.year}`
+// }
+
+// const myDate = new Date()
+// const myDateFormatted = formatDate(myDate)
+
+// console.log(myDateFormatted)
+
+// const userDate = new Date();
+//  userDate.setDate(userDate.getDate()-1);
+
+// console.log(userDate)
+
+//  function dateChange(){
+//     const currDate = new Date();
+//     return currDate.setDate(currDate.getDate()-1);
+// }
+
+// dateChange()
 
 router.post("/expense/seed", async(req,res)=>{
 
    let seedItems;
+   const endDate = new Date();
+            for (let i = 0; i < 3; i++){
+                
 
-   //not needed
-    // const newExpense = [
-
-    //     {
-    //         username: await User.findOne({username: "user1" }),
-    //         expensesentry:[
-    //             {
-    //                 amount: amount[Math.floor(Math.random() * amount.length)],
-    //                 category: category[Math.floor(Math.random() * category.length)],
-    //                 description: description[Math.floor(Math.random() * description.length)],
-                    
-    //             },
-    //             {
-    //                 amount: amount[Math.floor(Math.random() * amount.length)],
-    //                 category: category[Math.floor(Math.random() * category.length)],
-    //                 description: description[Math.floor(Math.random() * description.length)],
-                    
-    //             },
-    //             {
-    //                 amount: amount[Math.floor(Math.random() * amount.length)],
-    //                 category: category[Math.floor(Math.random() * category.length)],
-    //                 description: description[Math.floor(Math.random() * description.length)],
-                    
-    //             }
-    //         ]
-    //     }
+                    try {
+                        
+                         seedItems = await DataExpense.create({
     
-    // ];
-            
-            for (let i = 0; i < 9; i++){
+                            username: await User.findOne({username: "user1" }),
+                            expensesentry:[
+                                {
+                                    amount: amount[Math.floor(Math.random() * amount.length)],
+                                    category: category[Math.floor(Math.random() * category.length)],
+                                    description: description[Math.floor(Math.random() * description.length)],
+                                   
+                                    date: endDate.setDate(endDate.getDate()-i)
+                                    
+                                },
+                                {
+                                    amount: amount[Math.floor(Math.random() * amount.length)],
+                                    category: category[Math.floor(Math.random() * category.length)],
+                                    description: description[Math.floor(Math.random() * description.length)],
+                                    
+                                    date: endDate.setDate(endDate.getDate()-i)
+                                    
+                                },
+                                {
+                                    amount: amount[Math.floor(Math.random() * amount.length)],
+                                    category: category[Math.floor(Math.random() * category.length)],
+                                    description: description[Math.floor(Math.random() * description.length)],
+                                    
+                                    date: endDate.setDate(endDate.getDate()-i)
+                                    
+                                },
+    
+                            ]})
+                    
+                     } catch (err) {
+                             res.send(err.message);
+    
+                }
 
-                try {
+
                 
-                     seedItems = await DataExpense.create({
-
-                        username: await User.findOne({username: "user1" }),
-                        expensesentry:[
-                            {
-                                amount: amount[Math.floor(Math.random() * amount.length)],
-                                category: category[Math.floor(Math.random() * category.length)],
-                                description: description[Math.floor(Math.random() * description.length)],
-                                
-                            },
-                            {
-                                amount: amount[Math.floor(Math.random() * amount.length)],
-                                category: category[Math.floor(Math.random() * category.length)],
-                                description: description[Math.floor(Math.random() * description.length)],
-                                
-                            },
-                            {
-                                amount: amount[Math.floor(Math.random() * amount.length)],
-                                category: category[Math.floor(Math.random() * category.length)],
-                                description: description[Math.floor(Math.random() * description.length)],
-                                
-                            },
-
-                        ]})
                 
-                 } catch (err) {
-                         res.send(err.message);
-
-            }
-            
+        
         }
 
         res.send(seedItems);

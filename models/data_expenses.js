455d3aa01d6e494da5970ab282bba7068b918dd7
qamarularifin <qamarulafro89@gmail.com>
2021-12-24 23:01:00 +0800

    changed schema data_expenses back to org

--- models/data_expenses.js
+++ models/data_expenses.js
@@ -3,27 +3,27 @@ const Schema=mongoose.Schema;
 
 
 const dataExpensesSchema=new Schema({
-  // username: { // make reference to user 
-  //   type: Schema.Types.ObjectId,
-  //   ref: 'User',
-  //   required: true
-  // },
+  username: { // make reference to user 
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
   expensesentry:[
     {
  
-    //  date:{
-    //     type: Date,
-    //     default: Date.now,
-    //     required: true,
-    //  },
-    //  amount:{
-    //     type: Number,
-    //     required: true,
-    //  },
-    //  category:{
-    //     type: String,
-    //     required: true
-    //  },
+     date:{
+        type: Date,
+        default: Date.now,
+        required: true,
+     },
+     amount:{
+        type: Number,
+        required: true,
+     },
+     category:{
+        type: String,
+        required: true
+     },
      description:{
        type: String,
        required: true

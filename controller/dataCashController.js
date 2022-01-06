49152ae705bccbb406bfc35c0bf8d47992aaf2f2
LAPTOP-KH9KVP83\Suwen <ng.suwen@dhs.sg>
2022-01-07 00:59:30 +0800

    added cash and expense yearly data

--- controller/dataCashController.js
+++ controller/dataCashController.js
@@ -1,97 +1,128 @@
 // Dependencies
-const express = require("express")
-const mongoose = require("mongoose")
-const router = express.Router()
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
 
 // Schema
-const User = require("../models/user")
-const DataCash = require("../models/data_cash")
-
+const User = require("../models/user");
+const DataCash = require("../models/data_cash");
 
 // Routes
 // get all cash
-router.get("/", async(req, res) => {
-    let cash
-    try{
-      cash = await DataCash.find({})
-    }catch(error){
-      res.status(500).send({message: 'Unexpected Error'})
-      return
-    }
-    res.send(cash)
-  });
+router.get("/", async (req, res) => {
+  let cash;
+  try {
+    cash = await DataCash.find({});
+  } catch (error) {
+    res.status(500).send({ message: "Unexpected Error" });
+    return;
+  }
+  res.send(cash);
+});
 
-// get cash by username and monthly data 
-router.get("/user/:usernameid/:monthOfCash", async (req,res)=>{
-    const usernameid = req.params.usernameid
-    const monthOfCash = req.params.monthOfCash
-    const monthOfCashDateObj = new Date(monthOfCash)
-    // previous month's last day 2359
-    const firstday = new Date(monthOfCashDateObj.getFullYear(), monthOfCashDateObj.getMonth(),1)
-    // current month's last day 2359
-    const lastday = new Date(monthOfCashDateObj.getFullYear(), monthOfCashDateObj.getMonth() + 1, 1)
-    const cash = await DataCash.find({username:usernameid, 'cashentry.date':{'$gt': firstday, '$lte': lastday}})
-    res.send(cash)
-})
+// get cash by username and monthly data
+router.get("/user/:usernameid/:monthOfCash", async (req, res) => {
+  const usernameid = req.params.usernameid;
+  const monthOfCash = req.params.monthOfCash;
+  const monthOfCashDateObj = new Date(monthOfCash);
+  // previous month's last day 2359
+  const firstday = new Date(
+    monthOfCashDateObj.getFullYear(),
+    monthOfCashDateObj.getMonth(),
+    1
+  );
+  // current month's last day 2359
+  const lastday = new Date(
+    monthOfCashDateObj.getFullYear(),
+    monthOfCashDateObj.getMonth() + 1,
+    1
+  );
+  const cash = await DataCash.find({
+    username: usernameid,
+    "cashentry.date": { $gt: firstday, $lte: lastday },
+  });
+  res.send(cash);
+});
+
+// get cash by username and yearly data
+// YYYY-MM
+router.get("/user/:usernameid/yearly/:monthOfCash", async (req, res) => {
+  const usernameid = req.params.usernameid;
+  const monthOfCash = req.params.monthOfCash;
+  const monthOfCashDateObj = new Date(monthOfCash);
+  let cashArr=[]
+  for  (let i = 1; i<=12; i++){
+    let monthlyCash = 0
+    let firstday = new Date(
+      monthOfCashDateObj.getFullYear() - 1,
+      monthOfCashDateObj.getMonth() + i,
+      1
+    );
+    let lastday = new Date(
+      monthOfCashDateObj.getFullYear() - 1,
+      monthOfCashDateObj.getMonth() + i+1,
+      1
+    );
+    const cash = await DataCash.find({
+      username: usernameid,
+      "cashentry.date": { $gt: firstday, $lte: lastday },
+    });
+    cash.forEach((data)=>monthlyCash+=data.cashentry.amount)
+    cashArr.push(monthlyCash.toFixed(2))
+  }
+  res.send(cashArr);
+});
 
 // show route
-router.get("/:id", async (req,res)=>{
-    const {id} = req.params
-    const cash = await DataCash.findById(id).populate("username")
-    res.send(cash)
-})
-
-
-
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const cash = await DataCash.findById(id).populate("username");
+  res.send(cash);
+});
 
 // create route cash
 router.post("/", async (req, res) => {
-    let createdCash;
-    try{
-      createdCash = await DataCash.create(req.body);
-    }catch(err){
-      res.status(400).send({message: 'Invalid request body'})
-      return
-    }
-    res.send(createdCash);
-  });
+  let createdCash;
+  try {
+    createdCash = await DataCash.create(req.body);
+  } catch (err) {
+    res.status(400).send({ message: "Invalid request body" });
+    return;
+  }
+  res.send(createdCash);
+});
 
+router.delete("/:id", async (req, res) => {
+  let deletedCash;
+  try {
+    deletedCash = await DataCash.findByIdAndRemove(req.params.id);
+  } catch (err) {
+    res.status(400).send({ message: "Invalid request body" });
+    return;
+  }
+  res.send(deletedCash);
+});
 
-  router.delete("/:id", async(req,res)=>{
-      let deletedCash;
-      try{
-          deletedCash = await DataCash.findByIdAndRemove(req.params.id);
-      } catch(err){
-          res.status(400).send({message: "Invalid request body"})
-          return
-      }
-      res.send(deletedCash)
-  })
+router.get("/:id/edit", async (req, res) => {
+  let editedCash;
+  try {
+    editedCash = await DataCash.findById(req.params.id);
+  } catch (err) {
+    res.status(400).send({ message: "Invalid request body" });
+  }
+  res.send(editedCash);
+});
 
+router.put("/:id/edit", async (req, res) => {
+  let editedCash;
+  try {
+    editedCash = await DataCash.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+  } catch (err) {
+    res.status(400).send({ message: "Invalid request body" });
+  }
+  res.send(editedCash);
+});
 
-  router.get("/:id/edit", async(req,res)=>{
-    let editedCash;
-    try{
-        editedCash = await DataCash.findById(req.params.id)
-    } catch(err){
-        res.status(400).send({message: "Invalid request body"})
-    }
-    res.send(editedCash)
-})
-
-
-  router.put("/:id/edit", async(req,res)=>{
-      let editedCash;
-      try{
-          editedCash = await DataCash.findByIdAndUpdate(req.params.id, req.body, {new:true} )
-      } catch(err){
-          res.status(400).send({message: "Invalid request body"})
-      }
-      res.send(editedCash)
-  })
-
-
-
-
-module.exports = router
-
+module.exports = router;
--- controller/dataExpenseController.js
+++ controller/dataExpenseController.js
@@ -1,99 +1,130 @@
 // Dependencies
-const express = require("express")
-const mongoose = require("mongoose")
-const router = express.Router()
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
 
 // Schema
-const User = require("../models/user")
-const DataExpense = require("../models/data_expenses")
-
+const User = require("../models/user");
+const DataExpense = require("../models/data_expenses");
 
 // Routes
 // get all expense
-router.get("/", async(req, res) => {
-    let expense
-    try{
-      expense = await DataExpense.find({})
-    }catch(error){
-      res.status(500).send({message: 'Unexpected Error'})
-      return
-    }
-    res.send(expense)
+router.get("/", async (req, res) => {
+  let expense;
+  try {
+    expense = await DataExpense.find({});
+  } catch (error) {
+    res.status(500).send({ message: "Unexpected Error" });
+    return;
+  }
+  res.send(expense);
+});
+
+// get expense by username and monthly data
+router.get("/user/:usernameid/:monthOfExpense", async (req, res) => {
+  const usernameid = req.params.usernameid;
+  const monthOfExpense = req.params.monthOfExpense;
+  const monthOfExpenseDateObj = new Date(monthOfExpense);
+  // previous month's last day 2359
+  const firstday = new Date(
+    monthOfExpenseDateObj.getFullYear(),
+    monthOfExpenseDateObj.getMonth(),
+    1
+  );
+  // current month's last day 2359
+  const lastday = new Date(
+    monthOfExpenseDateObj.getFullYear(),
+    monthOfExpenseDateObj.getMonth() + 1,
+    1
+  );
+  const expense = await DataExpense.find({
+    username: usernameid,
+    "expensesentry.date": { $gt: firstday, $lte: lastday },
   });
+  res.send(expense);
+});
 
-
-// get expense by username and monthly data 
-router.get("/user/:usernameid/:monthOfExpense", async (req,res)=>{
-    const usernameid = req.params.usernameid
-    const monthOfExpense = req.params.monthOfExpense
-    const monthOfExpenseDateObj = new Date(monthOfExpense)
-    // previous month's last day 2359
-    const firstday = new Date(monthOfExpenseDateObj.getFullYear(), monthOfExpenseDateObj.getMonth(),1)
-    // current month's last day 2359
-    const lastday = new Date(monthOfExpenseDateObj.getFullYear(), monthOfExpenseDateObj.getMonth() + 1, 1)
-    const expense = await DataExpense.find({username:usernameid, 'expensesentry.date':{'$gt': firstday, '$lte': lastday}})
-    res.send(expense)
-})
-
+// get Expense by username and yearly data
+// YYYY-MM
+router.get("/user/:usernameid/yearly/:monthOfExpense", async (req, res) => {
+  const usernameid = req.params.usernameid;
+  const monthOfExpense = req.params.monthOfExpense;
+  const monthOfExpenseDateObj = new Date(monthOfExpense);
+  let expenseArr = [];
+  for (let i = 1; i <= 12; i++) {
+    let monthlyExpense = 0;
+    let firstday = new Date(
+      monthOfExpenseDateObj.getFullYear() - 1,
+      monthOfExpenseDateObj.getMonth() + i,
+      1
+    );
+    let lastday = new Date(
+      monthOfExpenseDateObj.getFullYear() - 1,
+      monthOfExpenseDateObj.getMonth() + i + 1,
+      1
+    );
+    const expense = await DataExpense.find({
+      username: usernameid,
+      "expensesentry.date": { $gt: firstday, $lte: lastday },
+    });
+    expense.forEach((data) => (monthlyExpense += data.expensesentry.amount));
+    expenseArr.push(monthlyExpense.toFixed(2));
+  }
+  res.send(expenseArr);
+});
 
 // show route
-router.get("/:id", async (req,res)=>{
-    const {id} = req.params
-    const expense = await DataExpense.findById(id).populate("username")
-    res.send(expense)
-})
-
-
-
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const expense = await DataExpense.findById(id).populate("username");
+  res.send(expense);
+});
 
 // create route expense
 router.post("/", async (req, res) => {
-    let createdExpense;
-    try{
-      createdExpense = await DataExpense.create(req.body);
-    }catch(err){
-      res.status(400).send({message: 'Invalid request body'})
-      return
-    }
-    res.send(createdExpense);
-  });
+  let createdExpense;
+  try {
+    createdExpense = await DataExpense.create(req.body);
+  } catch (err) {
+    res.status(400).send({ message: "Invalid request body" });
+    return;
+  }
+  res.send(createdExpense);
+});
 
+router.delete("/:id", async (req, res) => {
+  let deletedExpense;
+  try {
+    deletedExpense = await DataExpense.findByIdAndRemove(req.params.id);
+  } catch (err) {
+    res.status(400).send({ message: "Invalid request body" });
+    return;
+  }
+  res.send(deletedExpense);
+});
 
-  router.delete("/:id", async(req,res)=>{
-      let deletedExpense;
-      try{
-          deletedExpense = await DataExpense.findByIdAndRemove(req.params.id);
-      } catch(err){
-          res.status(400).send({message: "Invalid request body"})
-          return
-      }
-      res.send(deletedExpense)
-  })
+router.get("/:id/edit", async (req, res) => {
+  let editedExpense;
+  try {
+    editedExpense = await DataExpense.findById(req.params.id);
+  } catch (err) {
+    res.status(400).send({ message: "Invalid request body" });
+  }
+  res.send(editedExpense);
+});
 
+router.put("/:id/edit", async (req, res) => {
+  let editedExpense;
+  try {
+    editedExpense = await DataExpense.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+  } catch (err) {
+    res.status(400).send({ message: "Invalid request body" });
+  }
+  res.send(editedExpense);
+});
 
-  router.get("/:id/edit", async(req,res)=>{
-    let editedExpense;
-    try{
-        editedExpense = await DataExpense.findById(req.params.id)
-    } catch(err){
-        res.status(400).send({message: "Invalid request body"})
-    }
-    res.send(editedExpense)
-})
-
-
-  router.put("/:id/edit", async(req,res)=>{
-      let editedExpense;
-      try{
-          editedExpense = await DataExpense.findByIdAndUpdate(req.params.id, req.body, {new:true} )
-      } catch(err){
-          res.status(400).send({message: "Invalid request body"})
-      }
-      res.send(editedExpense)
-  })
-
-
-
-
-module.exports = router
-
+module.exports = router;
